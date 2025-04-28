d178d2dd43e56b03d28e34d2d1de4f9ab3b5e037
Joseph <joseph.kabinga@student.moringaschool.com>
2025-04-28 11:31:25 +0300

    third commit

---
+++ bot-battlr/src/YourBotArmy.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import BotCard from './BotCard';
+
+function YourBotArmy({ army, onRelease, onDischarge }) {
+  return (
+    <div className="your-army">
+      <h2>Your Bot Army</h2>
+      {army.length === 0 ? (
+        <p>No bots enlisted yet. Click on bots to add them to your army!</p>
+      ) : (
+        <div className="army-list">
+          {army.map(bot => (
+            <BotCard 
+              key={bot.id} 
+              bot={bot} 
+              onClick={() => onRelease(bot)}
+              onDischarge={() => onDischarge(bot)}
+              isInArmy={true}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default YourBotArmy;
